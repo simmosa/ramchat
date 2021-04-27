8938eccc7e80d34a75103f332aee91ef8a7df1d8
Simon <simonraj@bigpond.net.au>
2021-04-28 08:38:13 +1000

    readme and updated server code

---
+++ README.md
@@ -0,0 +1,52 @@
+# RAM chat 
+
+RAM chat is a web-based chat application for the user looking for simplicity and privacy. With no login or sign up required, users create a room name on the fly, allowing others to join in the room. User privacy is always maintained, as no data is kept on the server. The name, RAM chat, personifies the temporary nature of data associated with the chat. The conversation takes place, in the RAM of the user’s device. 
+
+### RAM chat link
+
+RAM chat can be found here: https://ram-chat.herokuapp.com/
+
+## Features
+
+With an intuitive interface, navigating the app is easy. The user is first prompted to create a room or join a room.
+
+Upon completing either choice, the user is faced with the chat window, which will alert of any activity, including users joining or leaving, name changes and the conversation.
+
+The hamburger menu will reveal more information and options. The cunning user, can monitor the number of live participants, reassuring privacy. A new alias can also be configured.
+
+## Development
+
+Node.js back-end and React front-end
+
+The project was a task in full stack web development. Practicing skills learned over the 12 week General Assembly, Software Engineering Immersive. The webserver was built using Express and included socket.io to handle communication between server and clients.
+
+React was used to build the client side. 
+
+The concept for RAM chat, stems from a desire to learn multiplayer/multiclient communication. Socket.io was outside the scope of the course and was great to pick a new technology.
+
+![](images/flowchart.png)
+
+### Code Overview
+
+The server has an array of room objects. When a room is created by the user, and room object is pushed onto the room array. 
+
+The room object is composed of room name and an array of client objects. 
+
+Each client has a socket object (created by socket.io), and an alias (name used by the client in the chat).
+
+The socket object has information that is used by socket.io when routing messages and communicating to clients. The socket object also has an id, that is used to authenticate the client to a room.
+
+
+## Difficulties and Challenges 
+
+A lack of clarity about how socket.io was going to work, made it difficult to plan the project. It was not clear what features were going to be needed for communication between clients or to guarantee authenticity. Early experiments to learn the technology, ended up being included in the final product
+
+As a first study on the topic, future projects will vastly be improved utilising socket.io’s library and concepts. It was a challenge to learn the socket.io methodology in time for the projects. As a result, socket.io was not utilised to its potential.
+
+The React front end did not completely utilise the React methodology. Components were used minimally. If time permits after the course, the code will be refactored to use components more.
+
+## Reflection
+
+Planning was lacking for this project, which would have made the code clearer and more concise to follow. The hope was to have enough time to refactor the code into a more modular structure. It is important to have good structure from the start of a project as time will always be lacking for refactoring. Good structure would also make the code more scalable in the future.
+
+All up, this was a great coding exercise to reaffirm what has been learned over the 12 week course.  
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { createSocket } = require('dgram');
 const express = require('express')
 const app = express()
 
@@ -16,41 +17,37 @@ httpServer.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
 
-// let registeredClients = []
-
 let rooms = []
 
 io.on('connection', (sock) => {
 
-    // sock.emit('welcome', 'Welcome. Start a chat or join a chat')
-
     sock.on('sendmessage', (txt, roomName) => {
         let foundRoom = rooms.filter(room => room.name === roomName)
-          
-        // verify the sock.id of client sending the message to make sure they are registered to that room. 
-        let verifiedClient = foundRoom[0].clients.filter(client => {
-            if (client.sock.id == sock.id) {
-                return client
-            } else {
-                return null
-            }
-        })
-        if (verifiedClient.length == 1) {
-            // send the message to all clients in the room
-            foundRoom[0].clients.forEach(client => {               
-                client.sock.emit('message', { message: txt, alias: verifiedClient[0].alias })               
+        
+        if (foundRoom.length == 1) { // if the room exists
+            // verify the sock.id of client sending the message to make sure they are registered to that room. 
+            let verifiedClient = foundRoom[0].clients.filter(client => {
+                if (client.sock.id == sock.id) {
+                    return client
+                } else {
+                    return null
+                }
             })
-        }
-
+            if (verifiedClient.length == 1) {
+                // if client is verified, send the message to all clients in the room
+                foundRoom[0].clients.forEach(client => {               
+                    client.sock.emit('message', { message: txt, alias: verifiedClient[0].alias })       
+                })
+            }
+        }   
     }) 
 
     sock.on('startRoom', (roomName) => {
-        let roomMatch = rooms.filter(room => room.name === roomName) // get all clients with the same code. There should be no match if the code is not already used.
-
+        let roomMatch = rooms.filter(room => room.name === roomName) // check to make sure roomName is not already in use
         if (roomMatch.length === 0) {
             let room = { 
                 name: roomName,
-                clients: [{sock, alias: 'host', leftRoom: false}]
+                clients: [{sock, alias: 'Host', leftRoom: false}]
             }
             rooms.push(room)
             sock.emit('successfulRegistration', {message: `Invite guests to join room: ${roomName}`, alias: '', announcement: true}) 
@@ -62,12 +59,12 @@ io.on('connection', (sock) => {
         let foundRoom = rooms.filter(room => room.name === roomName)
         
         if (foundRoom.length == 1) {
-            let aliasName = `guest ${foundRoom[0].clients.length}`
+            let aliasName = `Guest ${foundRoom[0].clients.length}`
             let joiningClient = {sock, alias: aliasName, leftRoom: false}
             
             foundRoom[0].clients.push(joiningClient)
 
-            sock.emit('successfulJoin', { message: `Welcome, ${aliasName}. You are now connected.`, alias: '' }) // send only to the joining client
+            sock.emit('successfulJoin', { message: `Welcome, ${aliasName}. You are now connected.`, aliasName, alias: '' }) // send only to the joining client
             
             foundRoom[0].clients.forEach(client => {
                 // send the new number of participants.
@@ -81,39 +78,62 @@ io.on('connection', (sock) => {
     })
 
     sock.on('changeAlias', (newAlias, roomName) => {
-
-        console.log("recieved newAlias, " + newAlias)
-        console.log("recieved roomName, " + roomName)
-
         let foundRoom = rooms.filter(room => room.name === roomName)
+            // find the room and verify the client is in that room
+        if (foundRoom.length == 1) {
+            let verifiedClient = foundRoom[0].clients.filter(client => {
+                if (client.sock.id == sock.id) {
+                    return client
+                } else {
+                    return null
+                }
+            })
+            if (verifiedClient.length == 1) {
+                let oldAlias = verifiedClient[0].alias
+                verifiedClient[0].alias = newAlias
+                // send the message to all clients in the room
+                foundRoom[0].clients.forEach(client => {               
+                    client.sock.emit('message', { message: `${oldAlias}, has changed their name to, ${newAlias}`, alias: 'announcement' })               
+                })
+            }
+        }
+    })
 
-        console.log(foundRoom)
-        console.log(foundRoom[0].name)
-
-        let verifiedClient = foundRoom[0].clients.filter(client => {
-            if (client.sock.id == sock.id) {
-                return client
-            } else {
-                return null
+    sock.on('disconnect', (message) => {
+        // remove the user from the room
+        rooms.forEach(room => { // check all rooms
+            for (let i = 0; i < room.clients.length; i++) { // clients array 
+                if (sock.id === room.clients[i].sock.id) {
+                    let alias = room.clients[i].alias
+                    room.clients = room.clients.filter((client,index) => index != i)
+                    notifyRoomOfExit(room, alias)
+                }
             }
         })
-        if (verifiedClient.length == 1) {
-            let oldAlias = verifiedClient[0].alias
-            console.log("old alias, " + oldAlias)
-            verifiedClient[0].alias = newAlias
-            // send the message to all clients in the room
-            foundRoom[0].clients.forEach(client => {               
-                client.sock.emit('message', { message: `${oldAlias} has changed their name to, ${newAlias}`, alias: 'announcement' })               
-            })
-        }
-
+        // let foundRoom = rooms.filter(room => room.name === roomName)
+        // if (foundRoom.length == 1) {
+        //     console.log("closing connection. Room found")
+        //     foundRoom[0].clients = foundRoom[0].clients.filter(client => {
+        //         if (client.sock.id == sock.id) {           
+        //             console.log("closing connection. user found. Removed from array")
+        //             return null
+        //         } else {}
+        //             return client
+        //         }
+        //     })
+        // } else {
+        //     console.log("connection closed. User was not in a room")
+        // }
+        // rooms = rooms.filter(room => {
+        //     room.clients.filter(client => client.sock.id != sock.id)
+        // })
     })
-
-    sock.on('disconnect', () => {
-        console.log("user disconnected")
-    })
-
-    
-
-
 })
+
+notifyRoomOfExit = (room, alias) => {
+    room.clients.forEach(client => {               
+        client.sock.emit('message', { message: `${alias} has left the room`, alias: 'announcement' })
+        client.sock.emit('participants', (room.clients.length))        
+    })
+} 
+
